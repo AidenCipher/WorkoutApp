2ff5d11b982426d8ed64caab63d8894bed2b49fb
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-10-20 11:23:12 +0530

    #172 17-10-21 recycler view to show history

--- app/src/main/java/com/aidencipher/workoutapp/HistoryActivity.kt
+++ app/src/main/java/com/aidencipher/workoutapp/HistoryActivity.kt
@@ -3,8 +3,11 @@ package com.aidencipher.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_bmiactivity.*
 import kotlinx.android.synthetic.main.activity_history.*
+import kotlinx.android.synthetic.main.dialog_custom_back_confirmation.*
 
 class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,8 +31,18 @@ class HistoryActivity : AppCompatActivity() {
         val dbHandler = SqliteOpenHelper(this, null)
         val allCompletedDatesList = dbHandler.getAllCompletedDatesList()
 
-        for(i in allCompletedDatesList){
-            Log.i("DateHISTORYACTIVITY", "" + i)
+        if(allCompletedDatesList.size > 0){
+            tvHistory.visibility = View.VISIBLE
+            rvHistory.visibility = View.VISIBLE
+            tvNoDataAvailable.visibility = View.GONE
+
+            rvHistory.layoutManager = LinearLayoutManager(this)
+            val historyAdapter = HistoryAdapter(this, allCompletedDatesList)
+            rvHistory.adapter = historyAdapter
+        }else{
+            tvHistory.visibility = View.GONE
+            rvHistory.visibility = View.GONE
+            tvNoDataAvailable.visibility = View.VISIBLE
         }
     }
 
